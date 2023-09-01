8d2b138b145c7aac65e247a5e416a554e1796242
subash <bhandarisubash22@gmail.com>
2023-09-01 20:52:53 +0545

    trial1

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Navbar />
       <Router>
         <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Homepage/>} />
           <Route path="/nepalmap" element={<NepalMap/>} />
   
         </Routes>
