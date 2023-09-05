fd757b8819795a51bee19f118711866c65974a86
subash <bhandarisubash22@gmail.com>
2023-09-05 22:26:16 +0545

    BrowserRouter removed

--- src/Forroute.js
+++ src/Forroute.js
@@ -1,18 +1,22 @@
 import Navbar from "./Component/Pages/Navigarion/Navbar.js";
 import Homepage from "./Component/Pages/Navigarion/Homepage.js";
 import Footer from "./Component/Pages/Navigarion/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import Routes and Route components
-import NepalMap from "./evpoint";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import Routes and Route components
+// import NepalMap from "./evpoint";
 import Home from "./Component/Pages/HomePage/Home";
 
 function Forroute() {
+    const router = createBrowserRouter([{ path: "/", element: <div>Test page</div> }]);
   return (
-    <Router>
-        <Routes>
-            <Route path="/" element={<Homepage/>}/>
-            <Route path="/nepalmap" element ={<NepalMap/>} />
-        </Routes>
-    </Router>
+    // <Router>
+    //     <Routes>
+    //         <Route path="/" element={<Homepage/>}/>
+    //         <Route path="/nepalmap" element ={<NepalMap/>} />
+    //     </Routes>
+    // </Router>
+<RouterProvider router={router}>
+ </RouterProvider >
+
   )
 }
 
