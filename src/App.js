a4d8f4674bbf4839e6b077d189d5e498a7ca4852
subash <bhandarisubash22@gmail.com>
2023-09-05 21:13:53 +0545

    after changing route

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import "./App.css";
-import Navbar from "./Component/Pages/Navigarion/Navbar.js";
-import Homepage from "./Component/Pages/Navigarion/Homepage.js";
-import Footer from "./Component/Pages/Navigarion/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import Routes and Route components
-import NepalMap from "./evpoint";
-import Home from "./Component/Pages/HomePage/Home";
+// import "./App.css";
+// import Navbar from "./Component/Pages/Navigarion/Navbar.js";
+// import Homepage from "./Component/Pages/Navigarion/Homepage.js";
+// import Footer from "./Component/Pages/Navigarion/Footer";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import Routes and Route components
+// import NepalMap from "./evpoint";
+// import Home from "./Component/Pages/HomePage/Home";
 
-function App() {
-  return (
-    <>
-      <Navbar />
+// function App() {
+//   return (
+//     <>
+//       <Navbar />
 
-      <Router>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/nepalmap" element={<NepalMap />} />
-        </Routes>
-      </Router>
-      <Footer />
-      {/* <Navbar/> */}
-      {/* <Homepage/>
-   <Router>
-   <Routes>
-<Route path='/' element={<Homepage/>}/>
-   <Route path="/nepalmap" element={<NepalMap/>} />
-   </Routes>
-   </Router>
-   <Footer/> */}
-    </>
-  );
-}
+//       <Router>
+//         <Routes>
+//           <Route path="/" element={<Homepage />} />
+//           <Route path="/nepalmap" element={<NepalMap />} />
+//         </Routes>
+//       </Router>
+//       <Footer />
+//       {/* <Navbar/> */}
+//       {/* <Homepage/>
+//    <Router>
+//    <Routes>
+// <Route path='/' element={<Homepage/>}/>
+//    <Route path="/nepalmap" element={<NepalMap/>} />
+//    </Routes>
+//    </Router>
+//    <Footer/> */}
+//     </>
+//   );
+// }
 
-export default App;
+// export default App;
